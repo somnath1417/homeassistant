474771e71fbc119ae7e4b36ae547cc4cc84a5e41
somnath1417 <patilsomnath1417@gmail.com>
2026-05-07 17:10:09 +0530

    add sence

--- custom_components/buildtrack/scene.py
+++ custom_components/buildtrack/scene.py
@@ -1,5 +1,10 @@
+import logging
+
 from homeassistant.components.scene import Scene
+
 from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(
@@ -8,7 +13,10 @@ async def async_setup_entry(
     async_add_entities,
     discovery_info=None,
 ):
+    """Set up BuildTrack scenes."""
+
     data = hass.data[DOMAIN][entry.entry_id]
+
     devices = data["devices"]
     api = data["api"]
 
@@ -16,18 +24,60 @@ async def async_setup_entry(
 
     for device in devices:
         if "SCENE" in device.get("type", []):
-            scenes.append(BuildTrackScene(device))
+            scenes.append(
+                BuildTrackScene(
+                    hass,
+                    api,
+                    device,
+                )
+            )
 
     async_add_entities(scenes)
 
 
 class BuildTrackScene(Scene):
+    """BuildTrack Scene Entity."""
 
-    def __init__(self, device):
+    def __init__(
+        self,
+        hass,
+        api,
+        device,
+    ):
+        self._hass = hass
+        self._api = api
         self._device = device
+
+        self._entity_id = device.get("entityId")
+        self._entity_key = device.get("entityKey")
+
         self._attr_name = device.get("entityName")
-        self._attr_unique_id = device.get("entityId")
+        self._attr_unique_id = self._entity_id
 
     async def async_activate(self, **kwargs):
-        # Later call your API here
-        pass
+        """Activate BuildTrack scene."""
+
+        _LOGGER.warning(
+            "Activating BuildTrack Scene | name=%s | entityId=%s",
+            self._attr_name,
+            self._entity_id,
+        )
+
+        response = await self._api.call(
+            endpoint=f"/activateScene/{self._entity_id}",
+            method="POST",
+            payload=None,
+        )
+
+        _LOGGER.warning(
+            "BuildTrack Scene Response | name=%s | response=%s",
+            self._attr_name,
+            response,
+        )
+
+        if response is None:
+            _LOGGER.error(
+                "Failed to activate BuildTrack Scene | name=%s | entityId=%s",
+                self._attr_name,
+                self._entity_id,
+            )

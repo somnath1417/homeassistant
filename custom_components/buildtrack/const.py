a3aca532b296113593291bb9b62715d13e82130c
somnath1417 <patilsomnath1417@gmail.com>
2026-05-13 12:55:31 +0530

    Update const.py

--- custom_components/buildtrack/const.py
+++ custom_components/buildtrack/const.py
@@ -1,7 +1,13 @@
 DOMAIN = "buildtrack"
-API_BASE_URL = "http://demobms.buildtrack.in/v1"
 
-TOKEN_URL = "https://demobms.buildtrack.in/btadmin/index.php/oauthtokenservice/token"
-DEVICES_URL = "http://demobms.buildtrack.in/v1/getDevices"
-AUTHORIZE_URL = "https://demobms.buildtrack.in/bms/btadmin/index.php/oauthtokenservice/authorize"
+CONF_API_URL = "api_url"
+CONF_AUTH_URL = "auth_url"
+CONF_AUTH_TYPE = "auth_type"
+
+CONF_CLIENT_ID = "client_id"
+CONF_CLIENT_SECRET = "client_secret"
+
+AUTH_TYPE_AUTH_CODE = "auth_code"
+AUTH_TYPE_CLIENT_CRED = "client_credentials"
+
 SCOPE = "devices"
